cb408666771f5ab955ab2ea2e261b1d0906c8861
Guilherme Quirino Maia <Guilherme.Maia@proway.treina>
2022-08-22 13:37:30 -0300

    creating login form

---
+++ lib/app/login_page_resolver.dart
@@ -0,0 +1,15 @@
+import 'package:micro_app_login/app/pages/login_page.dart';
+import 'package:micro_core/app/micro_app.dart';
+import 'package:micro_core/app/micro_core_utils.dart';
+
+class LoginPageResolver implements MicroApp {
+  @override
+  String get microAppName => 'micro_app_login';
+
+  @override
+  Map<String, WidgetBuilderArgs> get routes {
+    return {
+      '/login': ((context, args) => LoginPage()),
+    };
+  }
+}
---
+++ lib/app/pages/login_page.dart
@@ -0,0 +1,51 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:micro_core/micro_core.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextFormField(
+            controller: emailController,
+            decoration: const InputDecoration(hintText: 'Digite seu email'),
+            validator: (value) {
+              bool isValid = EmailValidator.validate(value.toString());
+              if (isValid == false) return 'Not a valid email';
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: emailController,
+            decoration: const InputDecoration(hintText: 'Digite sua senha'),
+            validator: (value) {
+              if (value!.length < 8) {
+                return 'Password too short';
+              } else if (!value.contains(
+                RegExp(r'[!@#$%^&*(),.?":{}|<>]'),
+              )) {
+                return 'Password should have an special character';
+              }
+              return null;
+            },
+          ),
+          DefaultButton(
+            onPressed: () {
+              navigatorKey.currentState?.pushNamed('/home');
+            },
+            text: 'Go to Home',
+          ),
+        ],
+      ),
+    );
+  }
+}
