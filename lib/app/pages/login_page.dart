21e4b552ab8e853a8c4df38fc516080d11dce3c6
Guilherme Quirino Maia <Guilherme.Maia@proway.treina>
2022-08-22 14:14:45 -0300

    retirando validadores form field

--- lib/app/pages/login_page.dart
+++ lib/app/pages/login_page.dart
@@ -1,4 +1,3 @@
-import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:micro_core/micro_core.dart';
 
@@ -20,42 +19,16 @@ class LoginPage extends StatelessWidget {
             TextFormField(
               controller: emailController,
               decoration: const InputDecoration(hintText: 'Digite seu email'),
-              validator: (value) {
-                bool isValid = EmailValidator.validate(emailController.text);
-                if (isValid == false) return 'Not a valid email';
-                return null;
-              },
             ),
             const SizedBox(height: 20),
             TextFormField(
               controller: passwordController,
               decoration: const InputDecoration(hintText: 'Digite sua senha'),
-              validator: (value) {
-                if (passwordController.text.length < 8) {
-                  return 'Password too short';
-                } else if (!passwordController.text.contains(
-                  RegExp(r'[!@#$%^&*(),.?":{}|<>]'),
-                )) {
-                  return 'Password should have an special character';
-                }
-                return null;
-              },
             ),
             const SizedBox(height: 20),
             DefaultButton(
               onPressed: () {
-                if (formKey.currentState!.validate()) {
-                  navigatorKey.currentState?.pushNamed('/home');
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text(
-                        'Algumas informações estão incorretas',
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  );
-                }
+                navigatorKey.currentState?.pushNamed('/home');
               },
               text: 'Go to Home',
             ),
