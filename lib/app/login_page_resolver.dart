fbde2630ce7f3551a452ea53311dc475d757a3bd
Guilherme Quirino Maia <Guilherme.Maia@proway.treina>
2022-08-22 13:44:03 -0300

    [feat] creating login page

--- lib/app/login_page_resolver.dart
+++ lib/app/login_page_resolver.dart
@@ -4,7 +4,7 @@ import 'package:micro_core/app/micro_core_utils.dart';
 
 class LoginPageResolver implements MicroApp {
   @override
-  String get microAppName => 'micro_app_login';
+  String get microAppName => '/micro_app_login';
 
   @override
   Map<String, WidgetBuilderArgs> get routes {
