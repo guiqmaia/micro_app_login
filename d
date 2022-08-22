b9988d84bc7e10924b8325bf0069a208bc23ed04
Guilherme Quirino Maia <Guilherme.Maia@proway.treina>
2022-08-22 14:12:18 -0300

    tentando arrumar form field

---
+++ d
@@ -0,0 +1,7 @@
+v.1.0.1+1       [fix] changing resolver class name
+v.1.0.2         [fix] changing resolver class name
+v1.0.0          creating login form
+v1.0.1          [feat] creating login page
+v1.0.3          [fix]arrumando problema de controller em text form field
+v1.0.4          [fix]fixing navigation at login page
+v1.0.5          [feat] solving form validator
--- lib/app/pages/login_page.dart
+++ lib/app/pages/login_page.dart
@@ -11,51 +11,56 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          TextFormField(
-            controller: emailController,
-            decoration: const InputDecoration(hintText: 'Digite seu email'),
-            validator: (value) {
-              bool isValid = EmailValidator.validate(value.toString());
-              if (isValid == false) return 'Not a valid email';
-              return null;
-            },
-          ),
-          TextFormField(
-            controller: passwordController,
-            decoration: const InputDecoration(hintText: 'Digite sua senha'),
-            validator: (value) {
-              if (value!.length < 8) {
-                return 'Password too short';
-              } else if (!value.contains(
-                RegExp(r'[!@#$%^&*(),.?":{}|<>]'),
-              )) {
-                return 'Password should have an special character';
-              }
-              return null;
-            },
-          ),
-          DefaultButton(
-            onPressed: () {
-              if (formKey.currentState!.validate()) {
-                navigatorKey.currentState?.pushNamed('/home');
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text(
-                      'Algumas informações estão incorretas',
-                      textAlign: TextAlign.center,
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(hintText: 'Digite seu email'),
+              validator: (value) {
+                bool isValid = EmailValidator.validate(emailController.text);
+                if (isValid == false) return 'Not a valid email';
+                return null;
+              },
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: passwordController,
+              decoration: const InputDecoration(hintText: 'Digite sua senha'),
+              validator: (value) {
+                if (passwordController.text.length < 8) {
+                  return 'Password too short';
+                } else if (!passwordController.text.contains(
+                  RegExp(r'[!@#$%^&*(),.?":{}|<>]'),
+                )) {
+                  return 'Password should have an special character';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 20),
+            DefaultButton(
+              onPressed: () {
+                if (formKey.currentState!.validate()) {
+                  navigatorKey.currentState?.pushNamed('/home');
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text(
+                        'Algumas informações estão incorretas',
+                        textAlign: TextAlign.center,
+                      ),
                     ),
-                  ),
-                );
-              }
-            },
-            text: 'Go to Home',
-          ),
-        ],
+                  );
+                }
+              },
+              text: 'Go to Home',
+            ),
+          ],
+        ),
       ),
     );
   }
