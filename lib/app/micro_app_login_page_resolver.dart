97f79704de8af37bca843fa2136a620c0b528dd2
Guilherme Quirino Maia <Guilherme.Maia@proway.treina>
2022-08-22 13:45:23 -0300

    [fix] changing resolver class name

--- lib/app/micro_app_login_page_resolver.dart
+++ lib/app/micro_app_login_page_resolver.dart
@@ -2,7 +2,7 @@ import 'package:micro_app_login/app/pages/login_page.dart';
 import 'package:micro_core/app/micro_app.dart';
 import 'package:micro_core/app/micro_core_utils.dart';
 
-class LoginPageResolver implements MicroApp {
+class MicroAppLoginPageResolver implements MicroApp {
   @override
   String get microAppName => '/micro_app_login';
 
