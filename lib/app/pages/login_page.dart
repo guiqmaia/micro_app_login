bce6447e2ed1e1412790e4072d2fdf0293623172
Guilherme Quirino Maia <Guilherme.Maia@proway.treina>
2022-08-22 16:17:35 -0300

    [feat] form validator

--- lib/app/pages/login_page.dart
+++ lib/app/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:micro_core/micro_core.dart';
 
@@ -10,29 +11,49 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextFormField(
-              controller: emailController,
-              decoration: const InputDecoration(hintText: 'Digite seu email'),
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              controller: passwordController,
-              decoration: const InputDecoration(hintText: 'Digite sua senha'),
-            ),
-            const SizedBox(height: 20),
-            DefaultButton(
-              onPressed: () {
-                navigatorKey.currentState?.pushNamed('/home');
-              },
-              text: 'Go to Home',
-            ),
-          ],
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(hintText: 'Digite seu email'),
+                validator: (value) {
+                  bool isValid = EmailValidator.validate(value!);
+                  if(!isValid) {
+                    return 'email must be valid';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(hintText: 'Digite sua senha'),
+                validator: (value) {
+                  if(value!.isEmpty) {
+                    return 'password must not be empty';
+                  } else if (value.length <= 6 ) {
+                    return 'password should have more than 6 characters';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              DefaultButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    navigatorKey.currentState?.pushNamed('/home');
+                  }
+                },
+                text: 'Go to Home',
+              ),
+            ],
+          ),
         ),
       ),
     );
